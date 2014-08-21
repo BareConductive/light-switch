b2569d86ca6a045792cf5b946a825782660963a6
Peter Krige <pete.krige@gmail.com>
2014-08-21 14:52:41 +0100

    Initial public commit

---
+++ Light_Switch/Light_Switch.ino
@@ -0,0 +1,80 @@
+/*******************************************************************************
+
+ Bare Conductive Light Switch Demo
+ ---------------------------------
+ 
+ Light_Switch.ino - simple touch light switch with toggle action (touch-on, 
+ touch-off)
+ 
+ Bare Conductive code written by Stefan Dzisiewski-Smith and Peter Krige.
+ 
+ This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 
+ Unported License (CC BY-SA 3.0) http://creativecommons.org/licenses/by-sa/3.0/
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+*******************************************************************************/
+
+#include <MPR121.h>
+#include <Wire.h>
+
+#define switchElectrode 11
+#define outputPin 13
+
+void setup(){
+  
+  pinMode(outputPin, OUTPUT);
+  digitalWrite(outputPin, LOW);
+  
+  Serial.begin(9600);
+  Wire.begin();
+  
+  // 0x5C is the MPR121 I2C address on the Bare Touch Board
+  if(!MPR121.begin(0x5C)){ 
+    Serial.println("error setting up MPR121");  
+    switch(MPR121.getError()){
+      case NO_ERROR:
+        Serial.println("no error");
+        break;  
+      case ADDRESS_UNKNOWN:
+        Serial.println("incorrect address");
+        break;
+      case READBACK_FAIL:
+        Serial.println("readback failure");
+        break;
+      case OVERCURRENT_FLAG:
+        Serial.println("overcurrent on REXT pin");
+        break;      
+      case OUT_OF_RANGE:
+        Serial.println("electrode out of range");
+        break;
+      case NOT_INITED:
+        Serial.println("not initialised");
+        break;
+      default:
+        Serial.println("unknown error");
+        break;      
+    }
+    while(1);
+  }
+  
+  // pin 4 is the MPR121 interrupt on the Bare Touch Board
+  MPR121.setInterruptPin(4);
+  // initial data update
+  MPR121.updateTouchData();
+}
+
+void loop(){
+  if(MPR121.touchStatusChanged()){
+    MPR121.updateTouchData();
+    if(MPR121.isNewTouch(switchElectrode)){
+      digitalWrite(outputPin, !digitalRead(outputPin));      
+    }  
+  }
+}
